19d3326510038755ea9d0c704c7202d6ec2b0927
Jennifer <jenniferromanmuerte@gmail.com>
2025-10-30 09:28:27 +0100

    basic js functionality completed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,33 @@ const textElement = document.querySelector('.js-message');
 
 
 const move = (option_player, option_computer) =>{
-
+  if(option_player === option_computer){
+    textElement.innerHTML = "Empate";
+  }
+  else if(option_player === "piedra"){
+    if(option_computer == "tijera"){
+      textElement.innerHTML = "Has ganado!";
+    }
+    else if(option_computer == "papel"){
+      textElement.innerHTML = "Has perdido";
+    }
+  }
+  else if(option_player === "papel"){
+     if(option_computer == "piedra"){
+      textElement.innerHTML = "Has ganado!";
+    }
+    else if(option_computer == "tijera"){
+      textElement.innerHTML = "Has perdido!";
+    }
+  }
+  else if(option_player === "tijera"){
+    if(option_computer == "papel"){
+      textElement.innerHTML = "Has ganado!";
+    }
+    else if(option_computer == "piedra"){
+      textElement.innerHTML = "Has perdido!";
+    }
+  }
 }
 
 const getRandomNumber = (max) => {
@@ -30,8 +56,9 @@ const optionComputer = (number_random) => {
 buttonElement.addEventListener('click',(ev)=>{
   ev.preventDefault();
   const option_player = optionElement.value;
+  console.log("Eleccion del jugador: " + option_player)
   const number_random = getRandomNumber(9);
   const option_computer = optionComputer(number_random);
-  console.log(option_computer);
+  console.log("Eleccion de la computadora: " + option_computer);
   move(option_player, option_computer);
 });
